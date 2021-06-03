5f3ec159fbf9c3a55bb02f98f6ee9922eec5418b
UnderTP <patrickjang@const.company>
2021-06-03 23:32:42 +0900

    First commit

--- src/views/MainPage/MainPage.js
+++ src/views/MainPage/MainPage.js
@@ -3,24 +3,25 @@ import axios from 'axios';
 
 import Portfolio from 'components/Portfolio'; 
 import logoImg from 'static/img/logo.png'; 
+import { array } from 'prop-types';
 
-
-const MainPage = () => {
-
-	const [userName, setUserName] = useState('-'); 
 
 	// TODO - STEP 1 =======================
 	// total amount와 portfolio에 해당하는 값을 가져와 화면에 출력해 주세요. 
 	// 또한 "차이"에 해당하는 값을 계산하여 화면에 출력해 주세요. 
+  
 
-	
+const MainPage = () => {
+
+	const [userName, setUserName] = useState('-'); 
 	const [totalAmount, setTotalAmount] = useState(0); 
 	const [userPortfolio, setUserPortfolio] = useState([]); 
+  const [amountDifference, setDifference] = useState(0); 
 
 
 	// componentDidMount 또는 componentDidUpdate와 같은 함수
 	useEffect(() => { 
-		getUserInfo('Ryan');
+		getUserInfo();
 	}, []); 
 
 	// user의 이름을 기준으로 데이터를 가져오는 함수
@@ -29,10 +30,19 @@ const MainPage = () => {
     axios.get(`/api/user/${name}`) // Express에서의 '/api/user/:name' API 참조
       .then(res => {
         if (res.data.result) {
-					const { name, portfolio } = res.data.result; 
+					const { name, portfolio, total_amount } = res.data.result; 
 					console.log("name: ", name); 
 					setUserName(name); // userName 값 갱신 
 					setUserPortfolio(portfolio); 
+          setTotalAmount(total_amount);
+          /*
+          setDifference(portfolio.reduce(sumAmount) - total_amount);
+          function sumAmount(total, num){
+            return total + num; 
+            
+          } */
+          // 차이값을 보여주기위해 Array reduce() 메소드를 이용해서 portfolio에 있는 "amount" 값들을 더하고 싶은데요
+          //portfolio.amount 한정으로 reduce()를 쓰는방법에서 막혔어요..! 
 				} 
       })
 			.catch(err => console.error(`Error: ${err}`)); 
@@ -59,19 +69,29 @@ const MainPage = () => {
       }}>
         <h3>(메인페이지 화면)</h3>
       </div>
+
 			<div style={{
 				height: '100px'
 			}}>
 				<button style={{
-				}}>Justin</button>
+				}} 
+        onClick={() => { getUserInfo("Justin") 
+        }}>Justin</button>
+
 				<button style={{
 					margin: '0 10px 0 10px'
-				}}>Ryan</button>
+				}}
+        onClick={() => { getUserInfo("Ryan") 
+        }}>Ryan</button>
+
 				<button style={{
-				}}>Owen</button>
+				}}
+        onClick={() => { getUserInfo("Owen") 
+        }}>Owen</button>
 			</div>
+
       <div style={{
-        height: '100px'
+        height: '30px'
       }}>
 				<span>이름: </span>
         <span style={{
@@ -80,6 +100,27 @@ const MainPage = () => {
 					fontSize: '20px'
 				}}>{userName}</span>
       </div>
+
+      <div style={{
+        height: '50px'
+      }}>
+        <span>Total Amount </span>
+        <span style={{
+					color: 'blue', 
+					fontSize: '20px'
+				}}>{totalAmount}</span>
+      </div>
+
+      <div style={{
+        height: '50px'
+      }}>
+        <span>차이:  </span>
+        <span style={{
+					color: 'blue', 
+					fontSize: '20px'
+				}}>{amountDifference}</span>
+      </div>
+
 			<div style={{
 				height: '50px'
 			}}>
